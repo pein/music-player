ad39a4316b80ec7d71396952f98963e4d5db5139
Ilyes Houha <mi.houha@gmail.com>
2020-12-08 19:09:33 +0100

    Show duration

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const artist = document.getElementById("artist");
 const audioElement = document.querySelector("audio");
 const progressContainer = document.getElementById("progress-container");
 const progress = document.getElementById("progress");
+const currentTimeEl = document.getElementById("current-time");
+const durationEl = document.getElementById("duration");
 const prevBtn = document.getElementById("prev");
 const playBtn = document.getElementById("play");
 const nextBtn = document.getElementById("next");
@@ -97,9 +99,21 @@ function updateProgressBar(e) {
   if (isPlaying) {
     const { duration, currentTime } = e.srcElement;
 
-    //Update Progress BarWidth
+    // Update Progress Bar Width
     const progressPercent = (currentTime / duration) * 100;
     progress.style.width = `${progressPercent}%`;
+
+    // Calculate display for Duration
+    const durationMinutes = Math.floor(duration / 60);
+    let durationSeconds = Math.floor(duration % 60);
+    if (durationSeconds < 10) {
+      durationSeconds = `0${durationSeconds}`;
+    }
+
+    // Delay switching Duration to avoid NaN
+    if (durationSeconds) {
+      durationEl.textContent = `${durationMinutes}:${durationSeconds}`;
+    }
   }
 }
 // Event Listners
