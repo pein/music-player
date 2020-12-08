3f9bb829c69fa4d878ef420ec6b61c8f7de89588
Ilyes Houha <mi.houha@gmail.com>
2020-12-08 14:50:33 +0100

    Songs List

--- script.js
+++ script.js
@@ -1,8 +1,34 @@
+const image = document.querySelector("img");
+const title = document.getElementById("title");
+const artist = document.getElementById("artist");
 const audioElement = document.querySelector("audio");
 const prevBtn = document.getElementById("pre");
 const playBtn = document.getElementById("play");
 const nextBtn = document.getElementById("next");
 
+// Music
+const songs = [
+  {
+    name: "jacinto-1",
+    displayName: "Electric Chill Machine",
+    artist: "Jacinto Design",
+  },
+  {
+    name: "jacinto-2",
+    displayName: "Seven Nation Army (Remix)",
+    artist: "Jacinto Design",
+  },
+  {
+    name: "jacinto-3",
+    displayName: "goodnight, disco Queen",
+    artist: "Jacinto Design",
+  },
+  {
+    name: "metric-1",
+    displayName: "Front Row (Remix)",
+    artist: "Metric/Jacinto Design",
+  },
+];
 // Check if Plying
 let isPlaying = false;
 
@@ -24,6 +50,7 @@ function pauseSong() {
   audioElement.pause();
 }
 
+// Play/Pause Event Listner
 playBtn.addEventListener("click", () => {
   isPlaying ? pauseSong() : playSong();
 });
