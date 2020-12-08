93d0916d02503b296a3ae91e54897c87ff71d8b9
Ilyes Houha <mi.houha@gmail.com>
2020-12-08 18:51:06 +0100

    Update ProgressBar Percentage

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const image = document.querySelector("img");
 const title = document.getElementById("title");
 const artist = document.getElementById("artist");
 const audioElement = document.querySelector("audio");
+const progressContainer = document.getElementById("progress-container");
+const progress = document.getElementById("progress");
 const prevBtn = document.getElementById("prev");
 const playBtn = document.getElementById("play");
 const nextBtn = document.getElementById("next");
@@ -89,6 +91,18 @@ function nextSong() {
 
 loadSong(songs[songIndex]);
 
+// Update ProgressBar And Time
+
+function updateProgressBar(e) {
+  if (isPlaying) {
+    const { duration, currentTime } = e.srcElement;
+
+    //Update Progress BarWidth
+    const progressPercent = (currentTime / duration) * 100;
+    progress.style.width = `${progressPercent}%`;
+  }
+}
 // Event Listners
 prevBtn.addEventListener("click", prevSong);
 nextBtn.addEventListener("click", nextSong);
+audioElement.addEventListener("timeupdate", updateProgressBar);
